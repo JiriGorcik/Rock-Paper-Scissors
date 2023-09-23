7571a58066ddec1830b63d727f75cc278ac5215b
JiriGorcik <jirigorcik@seznam.cz>
2023-09-23 22:33:31 +0200

    Adding game() which make 5 rounds of Rock-Paper-Scissors and decide who won

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let playerSelection = prompt("Rock, Paper or Scissors").toLocaleLowerCase()
+let playerScore = 0
+let computerScore = 0
+
+
+let getPlayerChoice = () => {
+   let answer = prompt("Rock, Paper or Scissors").toLowerCase()
+   return answer
+}   
 
 let getComputerChoice = () => {
 
     let choice = ["Rock", "Paper", "Scissors"]
     let answer = choice[Math.floor(Math.random()*3)]
-    return answer
+    return answer.toLowerCase()
 }
 
 let playRound =  (playerChoice, computerChoice) => {
-    playerChoice = playerSelection
-    computerChoice = getComputerChoice().toLowerCase()
+    
 
     if (playerChoice === computerChoice) {
         return "Tie"
     }
     else if (playerChoice === "rock" && computerChoice == "paper") {
-        return "You Lose, paper beats rock"
+        return "You Lose, paper beats rock" 
     }
     else if (playerChoice === "rock" && computerChoice == "scissors") {
         return "You Win, rock beats scissors"
@@ -31,6 +37,41 @@ let playRound =  (playerChoice, computerChoice) => {
     }
     else if (playerChoice === "scissors" && computerChoice == "paper") {
         return "You Win, scissors beats paper"
-    }
+    }  
 }
+
+let game = () => {
+    let playerScore = 0
+    let computerScore = 0
+
+    for (let i = 0; i < 5; i++){
+        let player = getPlayerChoice()
+        let comp = getComputerChoice()
+        console.log(playRound(player, comp));
+
+
+        if (playRound(player, comp).includes("Win")) {
+            playerScore++
+        } else if (playRound(player, comp).includes("Lose")) {
+            computerScore++
+        }
+        console.log(`${playerScore} : ${computerScore}`);
+        console.log("-----------");
+     }
+
+     console.log(`Game over`);
+
+     if (playerScore > computerScore) {
+        console.log("You are the Winner!!"); 
+     } else if (computerScore > playerScore) {
+        console.log("You lose, reload the page and try again"); 
+     } else {
+        console.log("Almost it's tie"); 
+     }
+}
+
+game()
+
+
+
 
