28e6599e92e1cd2d585b089916f5b59d8a598ec8
JiriGorcik <jirigorcik@seznam.cz>
2023-09-23 17:17:06 +0200

    Adding playRound function which decide who win

--- script.js
+++ script.js
@@ -7,3 +7,30 @@ let getComputerChoice = () => {
     return answer
 }
 
+let playRound =  (playerChoice, computerChoice) => {
+    playerChoice = playerSelection
+    computerChoice = getComputerChoice().toLowerCase()
+
+    if (playerChoice === computerChoice) {
+        return "Tie"
+    }
+    else if (playerChoice === "rock" && computerChoice == "paper") {
+        return "You Lose, paper beats rock"
+    }
+    else if (playerChoice === "rock" && computerChoice == "scissors") {
+        return "You Win, rock beats scissors"
+    }
+    else if (playerChoice === "paper" && computerChoice == "rock") {
+        return "You Win, paper beats rock"
+    }
+    else if (playerChoice === "paper" && computerChoice == "scissors") {
+        return "You Lose, scissors beats paper"
+    }
+    else if (playerChoice === "scissors" && computerChoice == "rock") {
+        return "You Lose, rock beats scissors"
+    }
+    else if (playerChoice === "scissors" && computerChoice == "paper") {
+        return "You Win, scissors beats paper"
+    }
+}
+
